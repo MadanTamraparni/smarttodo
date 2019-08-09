b4a3ef56faeca77a1cda08d4763ed4d3908da8dd
MadanTamraparni <46907375+MadanTamraparni@users.noreply.github.com>
2019-08-09 16:04:06 -0700

    Add files via upload

---
+++ SmartToDoServer/src/main/java/com/mtpb/smarttodo/SmartToDoServerApplication.java
@@ -0,0 +1,13 @@
+package com.mtpb.smarttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartToDoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartToDoServerApplication.class, args);
+	}
+
+}
